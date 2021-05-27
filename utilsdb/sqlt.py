32374f0d3640efa877eebbab1db710fec613612f
BillionZooms <thunderrock906@gmail.com>
2021-05-28 01:42:41 +0200

    Update sqlt.py

--- utilsdb/sqlt.py
+++ utilsdb/sqlt.py
@@ -41,7 +41,7 @@ async def maket(guild, member): #updates the boolean value "brushed" to True of 
             await crs.execute("""SELECT memberID, guildID, brushedt FROM ACTIVE""")
             vals = await crs.fetchall()
             for i in vals:
-                if member.id == i[0] and guild.id == i[1]:
+                if member.id == i[0] and guild.id == int(i[1]):
                     val = 1
                     await crs.execute(f"""UPDATE ACTIVE SET brushedt = {val} WHERE memberID = {i[0]}""")
                     await db.commit()
